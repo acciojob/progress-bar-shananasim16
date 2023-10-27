312b82976c85d786cbb58512b571fb0130814cad
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-28 02:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const circles = document.querySelectorAll(".circle");
+    const nextButton = document.getElementById("next");
+    const prevButton = document.getElementById("prev");
+    let currentCircle = 0;
+
+    nextButton.addEventListener("click", () => {
+        if (currentCircle < circles.length - 1) {
+            circles[currentCircle].classList.remove("active");
+            currentCircle++;
+            circles[currentCircle].classList.add("active");
+            prevButton.removeAttribute("disabled");
+        }
+
+        if (currentCircle === circles.length - 1) {
+            nextButton.setAttribute("disabled", "disabled");
+        }
+    });
+
+    prevButton.addEventListener("click", () => {
+        if (currentCircle > 0) {
+            circles[currentCircle].classList.remove("active");
+            currentCircle--;
+            circles[currentCircle].classList.add("active");
+            nextButton.removeAttribute("disabled");
+        }
+
+        if (currentCircle === 0) {
+            prevButton.setAttribute("disabled", "disabled");
+        }
+    });
+});
+
