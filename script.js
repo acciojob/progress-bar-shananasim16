8366fef2e906739365e4e9491f51a5cb6d87ee40
SHANA SHABNAM NASIM <142229568+shananasim16@users.noreply.github.com>
2023-10-30 19:27:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,89 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
-    const circles = document.querySelectorAll(".circle");
-    const nextButton = document.getElementById("next");
-    const prevButton = document.getElementById("prev");
-    let currentCircle = 0;
+const circles = document.querySelectorAll('.circle');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+let currentActive = 1;
 
-    nextButton.addEventListener("click", () => {
-        if (currentCircle < circles.length - 1) {
-            circles[currentCircle].classList.remove("active");
-            currentCircle++;
-            circles[currentCircle].classList.add("active");
-            prevButton.removeAttribute("disabled");
-        }
+nextButton.addEventListener('click', () => {
+  currentActive++;
 
-        if (currentCircle === circles.length - 1) {
-            nextButton.setAttribute("disabled", "disabled");
-        }
-    });
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
 
-    prevButton.addEventListener("click", () => {
-        if (currentCircle > 0) {
-            circles[currentCircle].classList.remove("active");
-            currentCircle--;
-            circles[currentCircle].classList.add("active");
-            nextButton.removeAttribute("disabled");
-        }
-
-        if (currentCircle === 0) {
-            prevButton.setAttribute("disabled", "disabled");
-        }
-    });
+  update();
 });
 
+prevButton.addEventListener('click', () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  if (currentActive === 1) {
+    prevButton.disabled = true;
+  } else if (currentActive === circles.length) {
+    nextButton.disabled = true;
+  } else {
+    prevButton.disabled = false;
+    nextButton.disabled = false;
+  }
+}
+
+
+// const lines = document.querySelectorAll('.line');
+// const prevBtn = document.getElementById('prev');
+// const nextBtn = document.getElementById('next');
+// let currActive = 1;
+
+// nextBtn.addEventListener('click', () => {
+//   currActive++;
+
+//   if (currActive > lines.length) {
+//     currActive = lines.length;
+//   }
+
+//   updateline();
+// });
+
+// prevButton.addEventListener('click', () => {
+//   currActive--;
+
+//   if (currActive < 1) {
+//     currActive = 1;
+//   }
+
+//   updateline();
+// });
+
+// function updateline() {
+//   lines.forEach((line, index) => {
+//     if (index < currActive) {
+//       line.classList.add('active');
+//     } else {
+//       line.classList.remove('active');
+//     }
+//   });
+
+//   if (currActive === 1) {
+//     prevBtn.disabled = true;
+//   } else if (currActive === lines.length) {
+//     nextBtn.disabled = true;
+//   } else {
+//     prevBtn.disabled = false;
+//     nextBtn.disabled = false;
+//   }
+// }
